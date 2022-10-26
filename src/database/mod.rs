1218c677d32e29f7fc59840511279647397e2b9a
JanKaul <jankaul@mailbox.org>
2022-10-26 19:36:57 +0200

    define migrations

---
+++ src/database/mod.rs
@@ -0,0 +1,45 @@
+use sea_orm::ConnectionTrait;
+use sea_orm::{Database, DbBackend, DbErr, Statement};
+use sea_orm_migration::prelude::*;
+
+mod migrator;
+
+const DATABASE_URL: &str = "postgres://postgres:postgres@localhost:5432";
+const DB_NAME: &str = "iceberg_catalog";
+
+pub(crate) async fn run() -> Result<(), DbErr> {
+    let db = Database::connect(DATABASE_URL).await?;
+
+    let db = &match db.get_database_backend() {
+        DbBackend::MySql => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
+        DbBackend::Postgres => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
+            ))
+            .await?;
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE \"{}\";", DB_NAME),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
+        DbBackend::Sqlite => db,
+    };
+
+    migrator::Migrator::refresh(db).await?;
+
+    Ok(())
+}
